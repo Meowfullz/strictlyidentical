583758cb5e2a4cd11f45bc2e0ee43f16798318c1
Gooby <129816575+Meowfullz@users.noreply.github.com>
2023-09-20 10:25:14 -0400

    first draft

--- main.py
+++ main.py
@@ -49,4 +49,26 @@ def main2():
 
 main2()
 
+def main3():
+	list1 = ['E', 'B', 'E', 'F', 'A', 'F', 'C']
+	list2 = ['E', 'B', 'E', 'F', 'A', 'F']
+	first=6
+	size=1
+	target='C'
+	i = 0
+	while i == 0:
 
+		target1 = search(list1,first,size,target)
+		# print (target1)
+		target2 = search(list2,first,size,target)
+		# print(target2)
+
+		if target1 == target2:
+			print('Two lists are strictly identical')
+		else: 
+			print('Two lists are not strictly identical')
+		i=+1
+	print (list1)
+	print (list2)
+
+main3()
